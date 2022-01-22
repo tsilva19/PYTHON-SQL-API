c5388d4553c244a9d7636462d91475aa6f0a73cc
Thiago <thiagoalves060@gmail.com>
2022-01-22 19:18:41 -0300

    fisrt commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+from item import Item, ItemList
+
+from security import authenticate, identity
+from user import UserRegister
+
+app = Flask(__name__)
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['SECRET_KEY'] = 'super-secret'
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
+
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
+
+if __name__ == '__main__':
+    app.run(debug=True)
